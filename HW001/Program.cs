12b5c55846d78f67ff14acc9bebe8c8c929ac124
Cos125 <val250640@mail.ru>
2022-10-02 09:58:35 +1000

    Task001

---
+++ HW001/Program.cs
@@ -0,0 +1,20 @@
+﻿int a, b, max;
+
+a = Random.Shared.Next(-10, 11);
+b = Random.Shared.Next(-10, 11);
+
+if (a > b)
+{
+    max = a;
+}
+else
+{
+    max = b;
+}
+
+Console.Write("a = ");
+Console.WriteLine(a);
+Console.Write("b = ");
+Console.WriteLine(b);
+Console.Write("max = ");
+Console.WriteLine(max);
