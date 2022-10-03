895825fefe2ced35f2ef7cacb1b2ea3f9efc07f1
Cos125 <val250640@mail.ru>
2022-10-03 19:15:18 +1000

    Homework02_Task01

---
+++ Seminar 2/HW001/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 10: Напишите метод, который принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+int num = Random.Shared.Next(100, 999);
+
+Console.WriteLine("num = "+num);
+
+void AverageNumber(int num)
+{
+    num /=10;
+    num%=10;
+    Console.WriteLine("Average number - "+num);
+}
+
+AverageNumber(num);
