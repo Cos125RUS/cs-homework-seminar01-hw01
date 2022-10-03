d9c1521e862356f1adea3b539d36d289487c23ff
Cos125 <val250640@mail.ru>
2022-10-03 20:08:15 +1000

    Homework02_Task03

---
+++ Seminar 2/HW003/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите метод, который принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+void Weekends()
+{
+    Console.Write("Enter the day (1-7): ");
+    int day = Convert.ToInt32(Console.ReadLine());
+
+    if (day > 5) Console.WriteLine("Weekends!");
+    else Console.WriteLine("Dull gray working day =(");
+}
+
+Weekends();
