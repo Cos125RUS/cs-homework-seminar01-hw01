6ba83e01d7c2a58e969c5a94fd8e043796c58ad7
Cos125 <val250640@mail.ru>
2022-10-03 19:42:53 +1000

    Homework02_Task02

---
+++ Seminar 2/HW002/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите метод, который выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.Write("num = ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+void ThirdNumber(int num)
+{
+    if (num < 100) 
+    {
+        Console.WriteLine("Your number does not have a third digit");
+    }
+    else
+    {
+        while (num > 1000) num /= 10;
+        Console.WriteLine("Third digit of your number is " + num%10);
+    }
+}
+
+ThirdNumber(num);
