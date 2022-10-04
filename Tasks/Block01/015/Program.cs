ba8476af884ceacf3aeec751a67d4eeab6271522
Cos125 <val250640@mail.ru>
2022-10-04 12:08:40 +1000

    Task015

---
+++ Tasks/Block01/015/Program.cs
@@ -0,0 +1,25 @@
+﻿// Дано число. Проверить кратно ли оно 7 и 23
+
+int num = Random.Shared.Next(100,999);
+Console.WriteLine("num = "+num);
+
+void Multiple(int num)
+{
+    int i = 0;
+
+    if (num % 7 == 0) 
+    {
+        Console.WriteLine(num + " multiple 7");
+        i++;
+    }
+
+    if (num % 23 == 0) 
+    {
+        Console.WriteLine(num + " multiple 23");
+        i++;
+    }
+
+    if (i == 0) Console.WriteLine (num + " not multiple 7 or 23");
+}
+
+Multiple(num);
