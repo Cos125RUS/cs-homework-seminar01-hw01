aca3d62689d476c2bbc2e9884187826acc09cd17
Cos125 <val250640@mail.ru>
2022-10-04 12:22:34 +1000

    Task016

---
+++ Tasks/Block01/016/Program.cs
@@ -0,0 +1,12 @@
+﻿// Дано число обозначающее день недели. Выяснить является номер дня недели выходным
+
+void Weekends()
+{
+    Console.Write("Enter the day (1-7): ");
+    int day = Convert.ToInt32(Console.ReadLine());
+
+    if (day > 5) Console.WriteLine("Weekends!");
+    else Console.WriteLine("Dull gray working day =(");
+}
+
+Weekends();
