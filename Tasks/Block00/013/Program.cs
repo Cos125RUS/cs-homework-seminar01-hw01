12bf10cbb677bb39ccbd4be2a2f4ebdf46a5f243
Cos125 <val250640@mail.ru>
2022-10-04 11:56:23 +1000

    Task014

--- Tasks/Block00/013/Program.cs
+++ Tasks/Block00/013/Program.cs
@@ -1,12 +1,12 @@
 ﻿// 13. Выяснить, кратно ли число заданному, если нет, вывести остаток.
 
 int num = Random.Shared.Next(1,10);
-Console.WriteLine("Предложенное число = "+num);
+Console.WriteLine("Computer number = "+num);
 
-Console.Write("Ваше число = ");
+Console.Write("Your number = ");
 int yourNum = Convert.ToInt32(Console.ReadLine());
 
 int res = yourNum % num;
 
-if (res == 0) Console.WriteLine(num+" кратно "+yourNum);
-else Console.WriteLine(num+" не кратно "+yourNum+". Остаток от деления = "+ res);
+if (res == 0) Console.WriteLine(num+" multiple "+yourNum);
+else Console.WriteLine(num+" not multiple "+yourNum+". Remainder of the division = "+ res);
---
+++ Tasks/Block00/014/Program.cs
@@ -0,0 +1,19 @@
+﻿// Найти третью цифру числа или сообщить, что её нет
+
+Console.Write("num = ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+void ThirdNumber(int num)
+{
+    if (num < 100) 
+    {
+        Console.WriteLine("Your number does not have a third digit");
+    }
+    else
+    {
+        while (num > 1000) num /= 10;
+        Console.WriteLine("Third digit of your number is " + num%10);
+    }
+}
+
+ThirdNumber(num);
