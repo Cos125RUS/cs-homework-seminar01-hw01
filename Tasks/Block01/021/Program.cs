7d4fe609996529216de9af94d7241618e9104d01
Cos125 <val250640@mail.ru>
2022-10-04 15:45:57 +1000

    Task021

---
+++ Tasks/Block01/021/Program.cs
@@ -0,0 +1,13 @@
+﻿// Программа проверяет пятизначное число на палиндромом.
+
+//int num = Random.Shared.Next(10000, 99999);
+//Console.WriteLine("Number = "+num);
+Console.Write("Number = ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+
+if (num / 10000 == num % 10)
+{
+    if (num/1000%10==num%100/10) Console.WriteLine("The number is a polyndrom");
+}
+else Console.WriteLine("The number is not a polyndrom");
