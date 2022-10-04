3f9c05de0368bebe0cbe116e28a7944fec26ee91
Cos125 <val250640@mail.ru>
2022-10-04 11:33:33 +1000

    Task013

---
+++ Tasks/Block00/013/Program.cs
@@ -0,0 +1,12 @@
+﻿// 13. Выяснить, кратно ли число заданному, если нет, вывести остаток.
+
+int num = Random.Shared.Next(1,10);
+Console.WriteLine("Предложенное число = "+num);
+
+Console.Write("Ваше число = ");
+int yourNum = Convert.ToInt32(Console.ReadLine());
+
+int res = yourNum % num;
+
+if (res == 0) Console.WriteLine(num+" кратно "+yourNum);
+else Console.WriteLine(num+" не кратно "+yourNum+". Остаток от деления = "+ res);
