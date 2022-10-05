0c3534320288f0f759c2a9f43e7968cf3e4fed72
Cos125 <val250640@mail.ru>
2022-10-05 19:28:07 +1000

    Task028

---
+++ Tasks/Block02/028/Program.cs
@@ -0,0 +1,18 @@
+﻿// Подсчитать сумму цифр в числе
+
+int num = new Random().Next(1, 100000000);
+Console.WriteLine("Number = " + num);
+
+int Sum(int n)
+{
+    if (n < 10) 
+    {
+        return n%10;
+    }
+    else 
+    {        
+        return (n%10 + Sum(n / 10));
+    }
+}
+
+Console.WriteLine("The sum of the digits in a number = " + Sum(num));
