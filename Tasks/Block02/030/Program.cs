4b5d52593000bd3c408415ab5fc6a630333b2cd1
Cos125 <val250640@mail.ru>
2022-10-05 19:40:35 +1000

    Task030

---
+++ Tasks/Block02/030/Program.cs
@@ -0,0 +1,12 @@
+﻿// Показать кубы чисел, заканчивающихся на четную цифру
+
+Console.Write("n = ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+double[] cube = new double[n];
+
+for (int i = 0; i < n; i++)
+{
+    cube[i] = Math.Pow((i + 1), 3);
+    if (cube[i]%10%2==0) Console.WriteLine((i+1)+"^3 = " + cube[i]);
+}
