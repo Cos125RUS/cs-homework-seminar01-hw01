09369ea46321e90544a604371787f727a11887a4
Cos125 <val250640@mail.ru>
2022-10-05 19:36:25 +1000

    Task029

---
+++ Tasks/Block02/029/Program.cs
@@ -0,0 +1,13 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N
+
+int n = new Random().Next(1, 50);
+Console.WriteLine("N = " + n);
+
+long multi = 1;
+
+for (int i = 1; i <= n; i++)
+{
+    multi *= i;
+}
+
+Console.WriteLine("Multiplication = " + multi);
