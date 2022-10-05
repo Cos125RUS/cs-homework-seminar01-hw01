318f4957c09d5dcc9dc7f24019cfd7bd595cde7e
Cos125 <val250640@mail.ru>
2022-10-05 19:23:08 +1000

    Task027

---
+++ Tasks/Block02/027/Program.cs
@@ -0,0 +1,18 @@
+﻿// Определить количество цифр в числе
+
+int num = new Random().Next(1, 100000000);
+Console.WriteLine("Number = " + num);
+
+int Size(int n)
+{
+    if (n < 10) 
+    {
+        return 1;
+    }
+    else 
+    {
+        return (1 + Size(n / 10));
+    }
+}
+
+Console.WriteLine("Size of number = " + Size(num));
