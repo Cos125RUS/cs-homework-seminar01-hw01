97fb8cb7414a51cc0940298598954c0dc18e4b1c
Cos125 <val250640@mail.ru>
2022-10-05 19:07:13 +1000

    Task026

---
+++ Tasks/Block02/026/Program.cs
@@ -0,0 +1,17 @@
+﻿// Возведите число А в натуральную степень B используя цикл
+
+int a = Random.Shared.Next(1,10);
+Console.WriteLine("A = "+a);
+
+int b = Random.Shared.Next(1,10);
+Console.WriteLine("B = "+b);
+
+int res = 1;
+
+while (b > 0)
+{
+    res *= a;
+    b--;
+}
+
+Console.WriteLine("A^B = "+res);
