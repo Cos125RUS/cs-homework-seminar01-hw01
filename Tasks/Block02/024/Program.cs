cf848aa180ff10a1e9f18093d21d3e0d4ef1e99f
Cos125 <val250640@mail.ru>
2022-10-05 18:49:23 +1000

    Task023

---
+++ Tasks/Block02/024/Program.cs
@@ -0,0 +1,12 @@
+﻿// Найти кубы чисел от 1 до N
+
+Console.Write("n = ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+double[] cube = new double[n];
+
+for (int i = 0; i < n; i++)
+{
+    cube[i] = Math.Pow((i + 1), 3);
+    Console.WriteLine((i+1)+"^3 = " + cube[i]);
+}
