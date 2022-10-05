39634e8126ca3e89d30a9bfd2aba8c7f2fb1bec9
Cos125 <val250640@mail.ru>
2022-10-05 18:59:36 +1000

    Task025

---
+++ Tasks/Block02/025/Program.cs
@@ -0,0 +1,12 @@
+﻿// Найти сумму чисел от 1 до А
+
+Console.Write("A = ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+int Sum (int a)
+{
+    if (a == 1) return 1;
+    else return (a + Sum(a-1));
+}
+
+Console.WriteLine(Sum(a));
