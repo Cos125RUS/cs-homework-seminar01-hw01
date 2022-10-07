bf589c08579c39512223323401d90e6bee548b97
Cos125 <val250640@mail.ru>
2022-10-07 20:51:54 +1000

    Task032

---
+++ Tasks/Block03/032/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран
+
+int size = 8;
+int[] array = new int[size];
+
+for (int i = 0; i < size; i++)
+{
+    array[i] = Random.Shared.Next(0,2);
+    Console.Write(array[i]+"  ");    
+}
