28597e2ad514402e3077b00fa561425fb7a825e4
Cos125 <val250640@mail.ru>
2022-10-07 20:50:07 +1000

    Task031

---
+++ Tasks/Block03/031/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задать массив из 8 элементов и вывести их на экран
+
+int size = 8;
+int[] array = new int[size];
+
+for (int i = 0; i < size; i++)
+{
+    array[i] = Random.Shared.Next(1,10);
+    Console.Write(array[i]+"  ");    
+}
