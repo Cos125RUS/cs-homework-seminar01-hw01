495945aac2de0a1cd3cc01fc36c17de29830e176
Cos125 <val250640@mail.ru>
2022-10-07 21:04:20 +1000

    Task034

---
+++ Tasks/Block03/034/Program.cs
@@ -0,0 +1,18 @@
+﻿// Написать программу замену элементов массива на противоположные
+
+int size = 12;
+int[] array = new int[size];
+
+for (int i = 0; i < size; i++)
+{
+    array[i] = Random.Shared.Next(0, 9);
+    Console.Write(array[i] + "  ");    
+}
+
+Console.WriteLine();
+
+for (int i = 0; i < size; i++)
+{    
+    array[i] *= -1;    
+    Console.Write(array[i] + " ");   
+}
