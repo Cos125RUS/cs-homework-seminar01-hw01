b56a913dd0c035747d548f748ae795b6fa09a295
Cos125 <val250640@mail.ru>
2022-10-10 12:15:30 +1000

    Sem3Task1

---
+++ Seminar 3/Task 01/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+int Enter()
+{
+    int num;
+    do
+    {
+        Console.Write("Number = ");
+        num = Convert.ToInt32(Console.ReadLine());
+        if (num < 10000) Console.Write("Invalid value. Try again.");
+    } while (num < 10000);
+
+    return num;
+}
+
+bool Polyndrom(int num)
+{
+    return ((num / 10000 == num % 10) && (num / 1000 % 10 == num % 100 / 10));
+}
+
+
+int num = Enter();
+if (Polyndrom(num) == true) Console.WriteLine("The number is a polyndrom");
+else Console.WriteLine("The number is not a polyndrom");
+
---
+++ Tasks/Block04/041/Program.cs
@@ -0,0 +1,24 @@
+﻿//41. Выяснить являются ли три числа сторонами треугольника
+
+int Enter()
+{
+    int size;
+    do
+    {
+        Console.Write("Number 1 = ");
+        size = Convert.ToInt32(Console.ReadLine());
+    } while (size < 1);
+    return size;
+}
+
+bool Triangle(int a, int b, int c)
+{
+    return (a + b > c && a + c > b && b + c > a);
+
+}
+
+int num1 = Enter();
+int num2 = Enter();
+int num3 = Enter();
+
+Console.WriteLine(Triangle(num1, num2, num3));
