b141eb35001b17cfe666a642db77acfa6eccdeb3
Cos125 <val250640@mail.ru>
2022-10-10 12:51:25 +1000

    Sem3TaskRomashka(1-2)

---
+++ Seminar 3/Task Romashka (option )/Program.cs
@@ -0,0 +1,60 @@
+﻿int[] Clients()
+{
+    int[] shop = new int[24];
+    int counter = 0;
+    string client;
+
+    Console.WriteLine("Введите временной промежуток для следующего клиента. Для окончания ввода введите (end)");
+    do
+    {
+        client = Console.ReadLine();
+        if (client != "end")
+        {
+            var pos = client.IndexOf('-');
+            int start = Convert.ToInt32(client.Substring(0, pos));
+            int end = Convert.ToInt32(client.Substring(pos + 1));
+            counter++;
+            for (int i = start; i < end; i++)
+            {
+                ++shop[i];
+            }
+        }
+    } while (client != "end");
+
+    //System.Console.WriteLine(String.Join(' ', shop));
+
+    return shop;
+}
+
+
+void MaxTime(int[] shop)
+{
+    int max = 0;
+
+    for (int i = 0; i < 24; i++)
+    {
+        if (shop[i] >= max)
+        {
+            max = shop[i];            
+        }
+    }
+
+    Console.Write("Максимальная загруженность: ");
+
+    for (int i = 0; i < 24; i++)
+    {
+        if (max == shop[i])
+        {
+            int j = i;
+            while (shop[j] == shop[j+1])
+            {
+                j++;
+            }
+            Console.Write(i + "-" + (j + 1) + "   ");
+            i = j;
+        }
+    }
+}
+
+int[] shop = Clients();
+MaxTime(shop);
---
+++ Seminar 3/Task Romashka (option 2)/Program.cs
@@ -0,0 +1,57 @@
+﻿void Counter(int[] romashka, int peaples)
+{
+    for (int i = 0; i < peaples; i++)
+    {
+        int coming, care;
+
+        do
+        {
+            Console.Write((i + 1) + " man come: ");
+            coming = Convert.ToInt32(Console.ReadLine());
+            if (coming <= 0 || coming > 23) Console.WriteLine("Wrong time");
+        } while (coming <= 0 || coming > 23);
+
+        do
+        {
+            Console.Write((i + 1) + " man out: ");
+            care = Convert.ToInt32(Console.ReadLine());
+            if (care < 0 || care > 23) Console.WriteLine("Wrong time");
+        } while (care < coming || care > 23);
+
+        for (int j = coming; j < care; j++)
+        {
+            ++romashka[j];
+        }
+    }
+    //System.Console.WriteLine(String.Join(' ', romashka));
+}
+
+void MaxTime(int[] romashka)
+{
+    int max = 0;
+
+    for (int i = 0; i < 24; i++)
+    {
+        if (romashka[i] >= max)
+        {
+            max = romashka[i];
+        }
+    }
+    for (int i = 0; i < 24; i++)
+    {
+        if (max == romashka[i])
+        {
+            Console.WriteLine("Maximum workload: " + i + "-" + (i + 1));
+        }
+    }
+}
+
+
+
+
+Console.Write("Peaples today - ");
+int peaples = Convert.ToInt32(Console.ReadLine());
+int[] romashka = new int[24];
+
+Counter(romashka, peaples);
+MaxTime(romashka);
