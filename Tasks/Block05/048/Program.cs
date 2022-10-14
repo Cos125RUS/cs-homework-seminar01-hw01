b23155cde4ac6a87dcb31a94d70b895fd4a67e80
Cos125 <val250640@mail.ru>
2022-10-14 12:55:25 +1000

    Task048

---
+++ Tasks/Block05/048/Program.cs
@@ -0,0 +1,35 @@
+﻿// Показать двумерный массив размером m×n заполненный целыми числами
+
+int[][] CreationX1(int n, int m)
+{
+    int[][] array = new int[n][m];
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+            array[i][j] = Random.Shared.Next(0, 100);
+    }
+    return array;
+}
+
+int Size()
+{
+    int number = Random.Shared.Next(0, 20);
+    return number;
+}
+
+void Print(int[][] array, int n, int m)
+{
+    for(int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        Console.Write(array[i][j] + ' ');
+    }
+    Console.WriteLine();
+}
+
+int n = Size(),
+    m = Size();
+
+int[][] array = Creation(n, m);
+
+Print(array, n, m);
