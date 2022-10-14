0a1f628ad1dd37e64d556755abb168dc5b3cc832
Cos125 <val250640@mail.ru>
2022-10-14 11:02:37 +1000

    Task047

---
+++ Tasks/Block04/047/Program.cs
@@ -0,0 +1,29 @@
+﻿// Написать программу копирования массива
+
+void NewArray(int[] array, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        array[i] = Random.Shared.Next(1, 100);
+    }
+}
+
+int[] Copy(int[] array, int n)
+{
+    int[] copyArray = new int[n];
+    for (int i = 0; i < n; i++)
+    {
+        copyArray[i] = array[i];
+    }
+    return copyArray;
+}
+
+int size = Random.Shared.Next(1, 20);
+int[] array = new int[size];
+
+NewArray(array, size);
+System.Console.WriteLine(string.Join(' ', array));
+
+int[] copyArray = Copy(array, size);
+// Copy(array, copyArray, size);
+System.Console.WriteLine(string.Join(' ', copyArray));
