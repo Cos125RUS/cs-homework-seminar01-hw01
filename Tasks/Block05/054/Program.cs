6a0998d9e4c98135426b4e492f0efbc3e0ad4fef
Cos125 <val250640@mail.ru>
2022-10-16 22:33:28 +1000

    Task054

---
+++ Tasks/Block05/054/Program.cs
@@ -0,0 +1,38 @@
+﻿// В матрице чисел найти сумму элементов главной диагонали
+
+
+int[,] FillArray()
+{
+    int n = new Random().Next(2, 10);    
+    int[,] arr = new int[n, n];
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            arr[i, j] = new Random().Next(2, 10);
+            Console.Write($"{arr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+    return arr;
+}
+
+int Sum(int[,] arr)
+{
+    int n = arr.GetLength(0),
+        sum = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (i == j ) sum += arr[i,j];           
+        }        
+    }
+    return sum;
+}
+
+int[,] array = FillArray();
+Console.WriteLine();
+int sum = Sum(array);
+System.Console.WriteLine("Sum = " + sum);
