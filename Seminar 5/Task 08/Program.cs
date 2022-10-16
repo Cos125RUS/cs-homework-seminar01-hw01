c1d80b7fd4be12dab6b3b62c90bcaa61bb345560
Cos125 <val250640@mail.ru>
2022-10-16 11:47:24 +1000

    HW0508

---
+++ Seminar 5/Task 08/Program.cs
@@ -0,0 +1,33 @@
+﻿// Найдите произведение пар чисел в одномерном массиве. Парой считаем первый и последний элемент, второй и предпоследний и т.д. Результат запишите в новом массиве.
+// [1 2 3 4 5] -> 5 8 3
+// [6 7 3 6] -> 36 21
+
+
+
+void Filling(int[] array, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        array[i] = Random.Shared.Next(1, 9);
+    }
+    System.Console.WriteLine(String.Join(' ', array));
+}
+
+void Multy(int[] array, int n)
+{
+    int[] multy = new int[n / 2];
+    for (int i = 0; i < n / 2; i++)
+    {
+        multy[i] = array[i] * array[n - 1 - i];
+    }
+    System.Console.WriteLine(String.Join("  ", multy));
+}
+
+Console.Write("Size of array = ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int[size];
+
+Filling(array, size);
+Console.WriteLine();
+Multy(array, size);
