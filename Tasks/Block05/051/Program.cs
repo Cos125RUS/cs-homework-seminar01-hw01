52c95e294001ecc145cda367914b5e04b5ddccb5
Cos125 <val250640@mail.ru>
2022-10-16 20:27:39 +1000

    Task051

---
+++ Tasks/Block05/051/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задать двумерный массив следующим правилом: Aₘₙ = m+n
+
+// int Size()
+// {
+//     int num = new Random().Next(2, 11);
+//     return num;
+// }
+
+int[,] FillArray()
+{
+    int m = new Random().Next(2, 11);
+    int n = new Random().Next(2, 11);
+    int[,] arr = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            arr[i, j] = i + j;
+            Console.Write($"{arr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+    return arr;
+}
+
+int[,] array = FillArray();
