b60ac73d1bc571bc88be344630e99388e0cbd7ee
Cos125 <val250640@mail.ru>
2022-10-18 12:45:23 +1000

    Task060(numbers)

---
+++ Tasks/Block05/060/Numbers/Numbers.cs
@@ -0,0 +1,56 @@
+﻿(int[,], int n, int m) FillArray()
+{
+    int n = new Random().Next(3, 9);
+    int m = new Random().Next(3, 9);
+
+    int[,] arr = new int[n, m];
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            arr[i, j] = new Random().Next(0, 10);
+        }
+    }
+    return (arr, n, m);
+}
+
+void PrintArray(int[,] arr, int n, int m)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            Console.Write($"{arr[i, j]}  ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int[] FrequencyAnalysis(int[,] arr, int n, int m)
+{
+    int[] frequency = new int[10];
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            ++frequency[arr[i, j]];
+        }
+    }
+    return frequency;
+}
+
+void PrintFrequency(int[] freq)
+{
+    for (int i = 0; i < 10; i++)
+    {
+        Console.WriteLine($"The number {i} occurs in the array - {freq[i]} times");
+    }
+}
+
+
+(int[,] array, int n, int m) = FillArray();
+PrintArray(array, n, m);
+int[] frequency = FrequencyAnalysis(array, n, m);
+PrintFrequency(frequency);
---
+++ Tasks/Block05/060/Text/Letters.cs
@@ -0,0 +1,16 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+string text = Console.ReadLine();
+System.Console.WriteLine();
+System.Console.WriteLine(text);
