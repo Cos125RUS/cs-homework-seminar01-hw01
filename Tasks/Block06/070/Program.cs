540c6a61a4b629bdbca04c7a58439aabd82062aa
Cos125 <val250640@mail.ru>
2022-10-19 20:36:42 +1000

    Task070

---
+++ Tasks/Block06/070/Program.cs
@@ -0,0 +1,21 @@
+﻿// Найти сумму цифр числа
+
+int sum = 0;
+
+int SumOfDigitsInNumber(int n)
+{
+    if (n == 0) return sum;
+    else
+    {
+        sum += n % 10;
+        return SumOfDigitsInNumber(n / 10);
+    }
+}
+
+
+
+
+Console.Write("Number = ");
+int num = Convert.ToInt32(Console.ReadLine());
+sum = SumOfDigitsInNumber(num);
+System.Console.WriteLine("Sum = " + sum);
