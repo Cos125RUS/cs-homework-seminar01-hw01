57b195c29c73415bd0a15fa37e78e1c0d76c851d
Cos125 <val250640@mail.ru>
2022-10-19 20:14:49 +1000

    Task068

---
+++ Tasks/Block06/068/Program.cs
@@ -0,0 +1,29 @@
+﻿// Показать натуральные числа от M до N, N и M заданы
+
+
+void Numbers(int n, int m)
+{
+    if (n == m)
+    {
+        System.Console.WriteLine(m);
+    }
+    else
+    {
+        System.Console.WriteLine(m);
+        Numbers(n, m + 1);
+    }
+}
+
+
+int n, m;
+do
+{
+    Console.Write("N = ");
+    n = Convert.ToInt32(Console.ReadLine());
+    Console.Write("M = ");
+    m = Convert.ToInt32(Console.ReadLine());
+    if (n < m) System.Console.WriteLine("Error! N < M. Try again.");
+} while (n < m);
+
+System.Console.WriteLine();
+Numbers(n, m);
