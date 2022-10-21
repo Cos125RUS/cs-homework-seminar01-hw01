9b872e1b838cfd56165da64e6a60197365de56e8
Cos125 <val250640@mail.ru>
2022-10-21 18:58:13 +1000

    Task072

---
+++ Tasks/Block06/072/Program.cs
@@ -0,0 +1,15 @@
+﻿// Написать программу возведения числа А в целую стень B
+
+int Exponentiation(int a, int b)
+{
+    if (b == 1) return a;
+    else
+        return a * Exponentiation(a, b - 1);
+}
+
+Console.Write("A = ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("B = ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("A^B = " + Exponentiation(a, b));
