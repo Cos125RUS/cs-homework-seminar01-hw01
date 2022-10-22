ec9cfb2954232df81da8e762a9826b35d25d2c06
Cos125 <val250640@mail.ru>
2022-10-22 17:22:41 +1000

    Task074v0.1

---
+++ Tasks/Block06/074/Program.cs
@@ -0,0 +1,73 @@
+﻿// В некотором машинном алфавите имеются четыре буквы «а», «и», «с» и «в». 
+// Покажите все слова, состоящие из n букв, которые можно построить из букв этого алфавита
+
+
+// 4^n вариантов
+string TheWords(char[] alphabet, int n, int size)
+{
+    // if (size > 0) TheWords (alphabet, n, size - 1);
+    // if (size > 0)
+    // {
+    //     TheWords(alphabet, n, size - 1);
+
+    //     for (int i = 0; i < n; i++)
+    //     {
+    //         Console.Write(alphabet[size - 1]);
+    //     }
+    //     Console.Write(' ');
+    // }
+
+
+
+
+
+    // for (int i = 0; i < n; i++)
+    // {
+    //     TheWords(alphabet, n - 1, size);
+    //     Console.Write(alphabet[size - 1]);
+    // }
+    //     Console.Write(' ');
+
+
+
+    // if (n == 0) Console.WriteLine(' ');
+    // else
+    // {
+    //     Console.Write(alphabet[size - 1]);
+    //     TheWords(alphabet, n - 1, size);
+    // }
+
+
+
+    // if (size > 0)
+    // {
+    //     TheWords(alphabet, n, size - 1);
+
+    //     for (int i = 0; i < n; i++)
+    //     {
+    //         Console.Write(alphabet[size - 1]);
+    //     }
+    //     Console.Write(' ');
+    // }
+}
+
+string[] DrawingUpTheAlphabet(char[] words, int length, int alphabetSize, int arraySize)
+{
+    string [] dictionary = new string[arraySize];
+    for (int i = 0; i < arraySize; i++)
+    {
+        dictionary[i] = TheWords()
+    }
+}
+
+int size = 4;
+char[] machineAlphabet = { 'а', 'и', 'с', 'в' };
+
+Console.Write("n = ");
+int length = Convert.ToInt32(Console.ReadLine());
+
+// TheWords(machineAlphabet, length, size);
+
+double arraySize = Math.Pow(size, n);
+string [] dictionary = DrawingUpTheAlphabet(machineAlphabet, length, size, arraySize);
+
