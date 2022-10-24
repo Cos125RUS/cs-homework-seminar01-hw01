c3e3f6c451d079872dc302fcb378e827e9511846
Cos125 <val250640@mail.ru>
2022-10-24 15:28:12 +1000

    Tetris.v0.1

--- Tetris/Program.cs
+++ Tetris/Program.cs
@@ -1,35 +1,106 @@
-// int[,] f = new int[25, 25];
-int[,] mapping = {{1,0,0,0},
-                  {1,1,0,0},
-                  {0,1,0,0},
-                  {0,0,0,0}};
+int[,] CreateField()
+{
+    int[,] f = new int[20, 20];
+    for (int i = 0; i < 19; i++)
+    {
+        f[i, 0] = 1;
+        f[i, 19] = 1;
+    }
+    for (int i = 0; i < 20; i++)
+    {
+        f[19, i] = 1;
+        f[0, i] = 1;
+    }
 
+    return f;
+}
 
+int[,] field = CreateField();
 
+void PrintField()
+{
+    for (int i = 0; i < 20; i++)
+        for (int j = 0; j < 20; j++)
+        {
+            Console.SetCursorPosition(20 + i, j);
+            if (field[i, j] == 1) Console.Write('+');
+        }
+}
+
+int[,] mapping = Copying();
+
+// Выбор следующей фигуры
+int[,] Choice(int n)
+{
+    int[,] shape1 = {{0,1,0,0},
+                     {1,1,0,0},
+                     {1,0,0,0},
+                     {0,0,0,0}};
+
+    int[,] shape2 = {{1,0,0,0},
+                     {1,0,0,0},
+                     {1,0,0,0},
+                     {1,0,0,0}};
+
+    int[,] shape3 = {{1,0,0,0},
+                     {1,0,0,0},
+                     {1,1,0,0},
+                     {0,0,0,0}};
+
+    int[,] shape4 = {{1,1,0,0},
+                     {1,1,0,0},
+                     {0,0,0,0},
+                     {0,0,0,0}};
+
+    int[,] shape5 = {{1,0,0,0},
+                     {1,1,0,0},
+                     {1,0,0,0},
+                     {0,0,0,0}};
+
+    switch (n)
+    {
+        case 1:
+            return shape1;
+
+        case 2:
+            return shape2;
+
+        case 3:
+            return shape3;
+
+        case 4:
+            return shape4;
+
+        default:
+            return shape5;
+    }
+}
+
+// Создание следующей фигуры
+int[,] Copying()
+{
+    int[,] shape = Choice(Random.Shared.Next(1, 6));
+    int[,] f = new int[4, 4];
+
+    for (int i = 0; i < 4; i++)
+        for (int j = 0; j < 4; j++)
+            f[i, j] = shape[i, j];
+
+    return f;
+}
+
+// Движение фигуры
 void Figure(int x, int y)
 {
     int k = 0,
         l = 0;
 
-    // for (int i = x - 1; i <= x + 2; i++)
-    // {
-    //     for (int j = y - 1; j <= y + 2; j++)
-    //     {
-    //         if (mapping[k, l] == 1) f[i, j] = 1;
-    //         l++;
-    //     }
-    //     k++;
-    // }
-
-    // Console.Clear();
     for (int i = x - 1; i <= x + 2; i++)
     {
         for (int j = y - 1; j <= y + 2; j++)
         {
-            Console.SetCursorPosition(i, j);
-          if (mapping[k, l] == 1) Console.Write("+");
-            // if (f[i, j] == 1) Console.Write("+");
-            // Console.Write("+");
+            Console.SetCursorPosition(20 + i, j);
+            if (mapping[k, l] == 1) Console.Write("+");
             l++;
         }
         l = 0;
@@ -37,22 +108,12 @@ void Figure(int x, int y)
     }
 }
 
-// Обнуление
-// void Zero()
-// {
-//     for (int i = 0; i < 25; i++)
-//     {
-//         for (int j = 0; j < 25; j++)
-//         {
-//             f[i, j] = 0;
-//         }
-//     }
-// }
 
 
 Console.CursorVisible = false;
 int x = 10;
 int y = 2;
+int gameOver = 17;
 
 
 
@@ -62,15 +123,15 @@ new Thread(() =>
     while (true)
     {
         Console.Clear();
-        Console.SetCursorPosition(0, 15);
-        System.Console.Write("+++++++++++++++++++++++++++++++++++++++");
+        Console.SetCursorPosition(20, 20);
+        PrintField();
         Figure(x, y);
         Thread.Sleep(500);
         y++;
-        if (y > 15)
+        if (y > gameOver)
         {
+            mapping = Copying();
             y = 1;
-            // Zero();
         }
     }
 }).Start();
@@ -82,12 +143,12 @@ while (true)
 
     if (key == ConsoleKey.LeftArrow)
     {
-        x--;
+        if (x > 2) x--;
         Figure(x, y);
     }
     if (key == ConsoleKey.RightArrow)
     {
-        x++;
+        if (x < 16) x++;
         Figure(x, y);
     }
     // if (key == ConsoleKey.Spacebar)
