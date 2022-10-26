39ac2eccbe3057675cbfd9b24357201113dc29ae
Cos125 <val250640@mail.ru>
2022-10-26 12:59:31 +1000

    Tetris.v1.4(Change_names)

--- Tetris/Program.cs
+++ Tetris/Program.cs
@@ -1,14 +1,18 @@
 // Вид стартового поля
-int[,] CreateField()
+int[,] CreateField(int horizontal, int vertical)
 {
-    int[,] f = new int[22, 20];
+    int[,] f = new int[vertical + 2, horizontal];
 
-    for (int i = 0; i < 20; i++)
+    for (int i = 0; i < vertical; i++)
     {
         f[i, 0] = 1;
+        f[i, horizontal - 1] = 1;
+    }
+
+    for (int i = 0; i < horizontal; i++)
+    {
         f[0, i] = 1;
-        f[i, 19] = 1;
-        for (int j = 19; j < 22; j++)
+        for (int j = vertical - 1; j < vertical + 2; j++)
             f[j, i] = 1;
     }
 
@@ -17,21 +21,21 @@ int[,] CreateField()
 
 
 // Отрисовка поля
-void PrintField(int[,] field)
+void PrintField(int[,] field, int horizontal, int vertical)
 {
-    for (int i = 0; i < 20; i++)
-        for (int j = 0; j < 20; j++)
+    for (int i = 0; i < vertical; i++)
+        for (int j = 0; j < horizontal; j++)
         {
             Console.SetCursorPosition(20 + i, j);
-            if (j == 0 || j == 19) Console.Write(Convert.ToChar(22));
-            if ((i == 0 || i == 19) && j > 0 && j < 19) Console.Write(Convert.ToChar(19));
-            if (field[i, j] == 1 && i > 0 && i < 19 && j > 0 && j < 19) Console.Write(Convert.ToChar(4));
+            if (j == 0 || j == horizontal - 1) Console.Write(Convert.ToChar(22));
+            if ((i == 0 || i == vertical - 1) && j > 0 && j < horizontal - 1) Console.Write(Convert.ToChar(19));
+            if (field[i, j] == 1 && i > 0 && i < vertical - 1 && j > 0 && j < horizontal - 1) Console.Write(Convert.ToChar(4));
         }
 }
 
 
 // Изменения поля
-void ChangeField(int x, int y, int[] lineCounter, int[,] field, int [,] mapping, int row, int column)
+void ChangeField(int x, int y, int[] lineCounter, int[,] field, int[,] mapping, int row, int column, int horizontal, int vertical)
 {
     for (int i = 0; i < row; i++)
         for (int j = 0; j < column; j++)
@@ -39,7 +43,7 @@ void ChangeField(int x, int y, int[] lineCounter, int[,] field, int [,] mapping,
             {
                 field[x + i, j + y - 1] = 1;
                 ++lineCounter[j + y];
-                if (lineCounter[j + y] == 18) Reduction(j + y, field, lineCounter);
+                if (lineCounter[j + y] == vertical - 2) Reduction(j + y, field, lineCounter, vertical);
             }
 }
 
@@ -114,19 +118,20 @@ int[,] Choice(int n)
 
 
 // Движение фигуры
-void Figure(int x, int y, int [,] mapping, int row, int column)
+void Figure(int x, int y, int[,] mapping, int row, int column)
 {
     for (int i = 0; i < row; i++)
         for (int j = 0; j < column; j++)
         {
             Console.SetCursorPosition(20 + i + x, j + y);
-            if (mapping[i, j] == 1) Console.Write(Convert.ToChar(4));
+            if (mapping[i, j] == 1 && y > 0) Console.Write(Convert.ToChar(4));
         }
 }
 
 
 // Проверка падения фигуры
-bool Drop(int x, int y, int[,] field, int [,] mapping, int row, int column, bool gameOver)
+bool gameOver = false;
+bool Drop(int x, int y, int[,] field, int[,] mapping, int row, int column)
 {
     for (int i = 0; i < row; i++)
         for (int j = 0; j < column; j++)
@@ -141,22 +146,22 @@ bool Drop(int x, int y, int[,] field, int [,] mapping, int row, int column, bool
 
 
 // Сокращение линий
-void Reduction(int line, int[,] field, int[] lineCounter)
+void Reduction(int line, int[,] field, int[] lineCounter, int vertical)
 {
     for (int i = line; i > 0; i--)
     {
-        for (int j = 1; j < 19; j++)
+        for (int j = 1; j < vertical - 1; j++)
             field[j, i] = field[j, i - 1];
         lineCounter[i] = lineCounter[i - 1];
     }
 
-    for (int i = 1; i < 19; i++)
+    for (int i = 1; i < vertical - 1; i++)
         field[i, 1] = 0;
 }
 
 
 // Проверка на врезаие в фигуры слева
-bool SideTest(int x, int y, int direction, int[,] field, int [,] mapping, int row, int column)
+bool SideTest(int x, int y, int direction, int[,] field, int[,] mapping, int row, int column)
 {
     for (int i = 0; i < row; i++)
         for (int j = 0; j < column; j++)
@@ -166,19 +171,22 @@ bool SideTest(int x, int y, int direction, int[,] field, int [,] mapping, int ro
 }
 
 
+// Параметры поля
+int horizontal = 20;
+int vertical = 15;
+
 // Инициализация поля
-int[,] field = CreateField();
-int[] lineCounter = new int[20];
+int[,] field = CreateField(horizontal, vertical);
+int[] lineCounter = new int[horizontal];
 
 // Первая запись выбранной фигуры
 (int[,] mapping, int row, int column) = Copying();
 
 // Начальные параметры
 Console.CursorVisible = false;
-int x = 10;
-int y = 2;
+int x = vertical / 2;
+int y = 0;
 int time = 500;
-bool gameOver = false;
 
 // Логика отрисовки всего
 new Thread(() =>
@@ -188,26 +196,26 @@ new Thread(() =>
         while (time > 500) { }
 
         Console.Clear();
-        Console.SetCursorPosition(20, 20);
-        PrintField(field);
+        Console.SetCursorPosition(20, vertical);
+        PrintField(field, horizontal, vertical);
         Figure(x, y, mapping, row, column);
         Thread.Sleep(time);
 
         if (gameOver)
         {
-            Console.SetCursorPosition(25, 22);
+            Console.SetCursorPosition(20 + vertical / 2 / 2, horizontal + 2);
             System.Console.WriteLine("GAME OVER!");
             break;
         }
 
         y++;
 
-        if (Drop(x, y, field, mapping, row, column, gameOver))
+        if (Drop(x, y, field, mapping, row, column))
         {
-            ChangeField(x, y, lineCounter, field, mapping, row, column);
+            ChangeField(x, y, lineCounter, field, mapping, row, column, horizontal, vertical);
             (mapping, row, column) = Copying();
-            y = 1;
-            x = 10;
+            y = 0;
+            x = vertical / 2;
             time = 500;
         }
     }
@@ -227,14 +235,14 @@ while (true)
 
     if (key == ConsoleKey.RightArrow)
     {
-        if (x < 19 - row && !SideTest(x, y, 1, field, mapping, row, column)) x++;
+        if (x < horizontal - 1 - row && !SideTest(x, y, 1, field, mapping, row, column)) x++;
         Figure(x, y, mapping, row, column);
     }
 
     if (key == ConsoleKey.Spacebar)
     {
         (mapping, row, column) = Twist(mapping, row, column);
-        while (x > 19 - row) x--;
+        while (x > horizontal - 1 - row) x--;
         while (x < 1) x++;
         Figure(x, y, mapping, row, column);
     }
@@ -250,7 +258,7 @@ while (true)
         if (time <= 500)
         {
             time = 99999999;
-            Console.SetCursorPosition(27, 22);
+            Console.SetCursorPosition(27, horizontal + 2);
             System.Console.WriteLine("PAUSE");
         }
         else time = 500;
