8434d7c7dd73376c5eb40bf0ec70921d3a735e4e
Cos125 <val250640@mail.ru>
2022-11-13 00:57:54 +1000

    HW_Sem9

---
+++ Seminar 9/Example 01/Program.cs
@@ -0,0 +1,45 @@
+﻿// Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+// Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+// Массив размером 2 x 2 x 2
+
+void FillArray(int[,,] arr, int n, int m, int h)
+{
+    int number = 10 + 2 * (2 * (n - 1) + (m - 1)) + (h - 1);
+
+    if (n > 0 && m > 0 && h > 0)
+        arr[n - 1, m - 1, h - 1] = number;
+
+    if (n > 0) FillArray(arr, n - 1, m, h);
+    if (m > 0) FillArray(arr, n, m - 1, h);
+    if (h > 0) FillArray(arr, n, m, h - 1);
+}
+
+void Print3DArray(int[,,] arr)
+{
+    int n = arr.GetLength(0);
+    int m = arr.GetLength(1);
+    int h = arr.GetLength(2);
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            for (int k = 0; k < h; k++)
+            {
+                Console.WriteLine($"({i}:{j}:{k}) - {arr[i, j, k]}   ");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+    }
+}
+
+
+int n = 2,
+    m = 2,
+    h = 2;
+
+int[,,] arr = new int[n, m, h];
+
+FillArray(arr, n, m, h);
+Print3DArray(arr);
---
+++ Seminar 9/Example 03/Program.cs
@@ -0,0 +1,18 @@
+﻿// Написать программу вычисления функции Аккермана
+
+
+int Ackermann(int m, int n)
+{
+    if (m == 0) return n + 1;
+    else
+        if (n == 0 && m > 0) return Ackermann(m - 1, 1);
+        else return Ackermann(m - 1, Ackermann(m, n - 1));
+}
+
+
+Console.Write("m = ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("n = ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.Write($"Ackermann ({m}, {n}) = {Ackermann(m, n)}");
